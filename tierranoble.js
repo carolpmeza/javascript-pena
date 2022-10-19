4c88d311d1687c1997bba0b6cae8a5d6ab3ae105
Carolina Pena <carolina_p.meza@hotmail.com>
2022-10-18 23:28:32 -0500

    updated code

--- tierranoble.js
+++ tierranoble.js
@@ -1,6 +1,7 @@
 //OBJETO CONSTRUCTOR DE PRODUCTOS
 class misProductos {
-    constructor(tipo, nombre, cantidad, precio) {
+    constructor(id, tipo, nombre, cantidad, precio) {
+        this.id = id;
         this.tipo = tipo;
         this.nombre = nombre;
         this.cantidad = cantidad;
@@ -14,39 +15,46 @@ class misProductos {
 
 
 //PRODUCTOS CONSTRUIDOS
-const producto0 = new misProductos("jabon", "cafe", "100gr", 70.00);
-const producto1 = new misProductos("jabon", "lavanda", "100gr", 60.00);
-const producto2 = new misProductos("jabon", "jazmin", "100gr", 60.00);
-const producto3 = new misProductos("jabon", "romero", "100gr", 80.00);
-const producto4 = new misProductos("jabon", "miel", "100gr", 80.00);
-const producto5 = new misProductos("jabon", "bergamota", "100gr", 85.00);
-const producto6 = new misProductos("salsa", "nuez", "300gr", 170.00);
-const producto7 = new misProductos("salsa", "morita", "300gr", 170.00);
-const producto8 = new misProductos("salsa", "almendra", "300gr",180.00);
-const producto9 = new misProductos("salsa", "chiltepin", "300gr",190.00);
-const producto10 = new misProductos("salsa", "pistache", "300gr",190.00);
+const producto0 = new misProductos(0,"jabon", "cafe", "100gr", 70.00);
+const producto1 = new misProductos(1,"jabon", "lavanda", "100gr", 60.00);
+const producto2 = new misProductos(2,"jabon", "jazmin", "100gr", 60.00);
+const producto3 = new misProductos(3,"jabon", "romero", "100gr", 80.00);
+const producto4 = new misProductos(4,"jabon", "miel", "100gr", 80.00);
+const producto5 = new misProductos(5,"jabon", "bergamota", "100gr", 85.00);
+const producto6 = new misProductos(6,"salsa", "nuez", "300gr", 170.00);
+const producto7 = new misProductos(7,"salsa", "morita", "300gr", 170.00);
+const producto8 = new misProductos(8,"salsa", "almendra", "300gr",180.00);
+const producto9 = new misProductos(9,"salsa", "chiltepin", "300gr",190.00);
+const producto10 = new misProductos(10,"salsa", "pistache", "300gr",190.00);
+
+//ARRAY
+const productos = [producto0, producto1,producto2,producto3,producto4,producto5,producto6,producto7,producto8,producto9,producto10];
 
 
 //PROMPT CONDICIONAL TEST DE RECOMENDACIONES
-let recomend = prompt("Se considera usted una persona que disfruta de experiencias: A)bienestar B)gastronomicas");
+let recomend = prompt("Se considera usted una persona que disfruta de experiencias: A) bienestar B) gastronomicas");
 
-    if ( recomend.toLowerCase =="bienestar") {
+    if ( recomend.toLowerCase == "bienestar") {
         alert("Te recomendamos nuestra seleccion de jabones artesanales para el autocuidado. Todos aportan diferentes beneficios. CONOCELOS!");
     }
 
-    if (recomend.toLowerCase =="gastronomicas") {
-        alert("Te recomendamos nuestra seleccion de salsas gourmet, con exquisitos frutos secos y sabores unicos que te sorprenderan con todas tus comidas. PRUEBALAS!")
+    if (recomend.toLowerCase == "gastronomicas") {
+        alert("Te recomendamos nuestra seleccion de salsas gourmet, con exquisitos frutos secos y sabores unicos que te sorprenderan con todas tus comidas. PRUEBALAS!");
     }
 
     else{
         alert("Nuestros productos son artesanales, hechos con ingredientes organicos de alta calidad. SEGURO ENCONTRARAS ALGO QUE TE ENCANTARA!!")
     }
 
-//SELECCIONAR PRODUCTOS
-    let muestraProductos = "Seleccione el producto deseado\n"
+//SELECCIONAR PRODUCTOS DE LISTA
+    let msjListaProductos = "Seleccione el id# del producto deseado\n"
 
-    for (producto of productos){
-        muestraProductos += `${producto.tipo} - ${producto.nombre} - ${producto.cantidad} - $${producto.precio}\n`;
+    for (componente of productos) {
+        msjListaProductos += `${componente.id} - ${componente.tipo} - ${componente.nombre} - ${componente.cantidad} - $${componente.precio}\n`;
     }
 
-    let seleccionProd = parseInt(prompt(muestraProductos);)
+    let selectAProd = parseInt(prompt(msjListaProductos));
+
+    const selectedProd = productos.find(componente => componente.id == selectAProd);
+
+    alert('Se ha agregado ${selectedProd.tipo} - {selectedProd.nombre} a tu lista de compras')
