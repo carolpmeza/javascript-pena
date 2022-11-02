82975a6f662028ba3ea3dec3a08abd5c1e132903
Carolina Pena <carolina_p.meza@hotmail.com>
2022-11-02 14:32:33 -0600

    add product catalog section

--- desafio3.js
+++ desafio3.js
@@ -31,6 +31,7 @@ const productos = [producto0, producto1,producto2,producto3,producto4,producto5]
 
 //Tarjetas de los productos
 function renderBootStrap(){
+    let cardContainer = document.querySelector(".catProductos");
     for (elem of productos){
         let card = document.createElement("div")
 
@@ -44,7 +45,7 @@ function renderBootStrap(){
                 <input type="button" onClick="agregarCarrito(${elem.id})" href="#" class="btn btn-secondary addToCart" value="Comprar">
             </div>
         </div>`
-        document.body.append(card)
+        cardContainer.appendChild(card)
 }
 }
 
@@ -118,35 +119,8 @@ function addToCartClicked(event){
     console.log( itemTitle, itemPrice)
 
 
-    addItemToShoppingCart(itemTitle, itemPrice, itemImage);
+    // addItemToShoppingCart(itemTitle, itemPrice, itemImage);
 }
 
-function addItemToShoppingCart(itemTitle, itemPrice, itemImage){
-    const shoppingCartRow = document.createElement("div");
-    const shoppingCartContent =`
-    <div class="row shoppingCartItem">
-        <div class="col-6">
-            <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                <img src=${itemImage} class="shopping-cart-image">
-                <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}</h6>
-            </div>
-        </div>
-        <div class="col-2">
-            <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                <p class="item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
-            </div>
-        </div>
-        <div class="col-4">
-            <div
-                class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
-                <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
-                    value="1">
-                <button class="btn btn-danger buttonDelete" type="button">X</button>
-            </div>
-        </div>
-    </div>`;
-    shoppingCartRow.innerHTML = shoppingCartContent
-    shoppingCartItemsContainer.append(shoppingCartRow);
 
-}
 
